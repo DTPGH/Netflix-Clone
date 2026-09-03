98c419fc2f0e6ddf66b91e20e7a31caefb80fc68
DatPGH <datphan0602@gmail.com>
2026-09-03 11:18:48 +0700

    chore: configure database context dependency injection

---
+++ src/NetflixClone.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using NetflixClone.Infrastructure.Persistence;
+
+namespace NetflixClone.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, string connectionString)
+    {
+        services.AddDbContext<NetflixCloneDbContext>(options =>
+            options.UseSqlServer(connectionString));
+            
+        return services;
+    }
+}
